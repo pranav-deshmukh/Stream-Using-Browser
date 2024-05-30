d24e2582a29f7a7f4b0a73320deaee9c681ccd82
Pranav Deshmukh <pranavdeshmukh190@gmail.com>
2024-05-30 10:45:58 +0530

    feat:live stream to yt

--- index.js
+++ index.js
@@ -1,11 +1,61 @@
 import http from "http";
 import path from "path";
 import express from "express";
+import { spawn } from "child_process";
 import { Server as SocketIo } from "socket.io";
 
 const app = express();
 const server = http.createServer(app);
 const io = new SocketIo(server);
+
+const options = [
+  "-i",
+  "-",
+  "-c:v",
+  "libx264",
+  "-preset",
+  "ultrafast",
+  "-tune",
+  "zerolatency",
+  "-r",
+  `${25}`,
+  "-g",
+  `${25 * 2}`,
+  "-keyint_min",
+  25,
+  "-crf",
+  "25",
+  "-pix_fmt",
+  "yuv420p",
+  "-sc_threshold",
+  "0",
+  "-profile:v",
+  "main",
+  "-level",
+  "3.1",
+  "-c:a",
+  "aac",
+  "-b:a",
+  "128k",
+  "-ar",
+  128000 / 4,
+  "-f",
+  "flv",
+  `rtmp://a.rtmp.youtube.com/live2/`,
+];
+
+const ffmpegProcess = spawn("ffmpeg", options);
+
+ffmpegProcess.stdout.on("data", (data) => {
+  console.log(`ffmpeg stdout: ${data}`);
+});
+
+ffmpegProcess.stderr.on("data", (data) => {
+  console.error(`ffmpeg stderr: ${data}`);
+});
+ffmpegProcess.on("close", (code) => {
+  console.log(`ffmpeg process exited with code: ${code}`);
+});
 
 app.use(express.static(path.resolve("./public")));
 
@@ -13,6 +63,9 @@ io.on("connection", (socket) => {
   console.log("Socket Connected", socket.id);
   socket.on("binary stream", (stream) => {
     console.log("binary stream incoming...");
+    ffmpegProcess.stdin.write(stream, (err) => {
+      console.log("Err", err);
+    });
   });
 });
 
